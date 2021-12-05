5f1f78a2ca3fb24f7576970bd507a7bf484764c0
Jessie Li <93940167+jessieli11@users.noreply.github.com>
2021-12-05 05:00:16 +0000

    part 1

--- part1.py
+++ part1.py
@@ -16,12 +16,14 @@ The smallest number is 2
 '''
 
 number = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+number3 = int(input("Enter another number: "))
+
 
 smallest = number
-
-number = int(input("Enter another number: "))
-
-if number < smallest:
-  smallest = number
+if number2 <= smallest:
+  smallest = number2
+elif number3 <= smallest:
+  smallest = number3 
   
 print("The smallest number is ", smallest)
